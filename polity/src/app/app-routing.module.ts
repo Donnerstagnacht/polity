88cabce5faa6b9e50e3ba58fb5b76421ee504143
Donnerstagnacht <tobias.hassebrock@gmail.com>
2023-10-15 00:46:03 +0200

    store and router

--- polity/src/app/app-routing.module.ts
+++ polity/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LoginComponent} from "./core/login/login.component";
+import {RegisterComponent} from "./core/register/register.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent
+  },
+  {
+    path: 'signup',
+    component: RegisterComponent
+  },
+  {
+    path: '**',
+    component: LoginComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
