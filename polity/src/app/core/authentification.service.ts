a02b538d597eabe0cc31c3f99a75b2efc17b3ab8
Donnerstagnacht <tobias.hassebrock@gmail.com>
2023-10-14 16:03:20 +0200

    Basic supabase setup

---
+++ polity/src/app/core/authentification.service.ts
@@ -0,0 +1,66 @@
+import { Injectable } from '@angular/core';
+import {AuthSession, createClient, SignInWithPasswordCredentials, SupabaseClient, User} from "@supabase/supabase-js";
+import {environment} from "../../environments/environment";
+
+export interface Profile {
+  id?: string
+  username: string
+  website: string
+  avatar_url: string
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthentificationService {
+  private supabase: SupabaseClient
+  _session: AuthSession | null = null
+
+  constructor() {
+    this.supabase = createClient(environment.supabaseUrl, environment.supabaseKey);
+  }
+
+  get session(): AuthSession | null {
+    this.supabase.auth.getSession().then(({ data }) => {
+      this._session = data.session;
+    })
+    return this._session;
+  }
+
+  profile(user: User) {
+    return this.supabase
+      .from('profiles')
+      .select(`username, website, avatar_url`)
+      .eq('id', user.id)
+      .single();
+  }
+
+  signUp(credentials: SignInWithPasswordCredentials) {
+    return this.supabase.auth.signUp(credentials);
+  }
+  signIn(credentials: SignInWithPasswordCredentials) {
+    return this.supabase.auth.signInWithPassword(credentials);
+  }
+
+  signOut() {
+    return this.supabase.auth.signOut()
+  }
+
+  updateProfile(profile: Profile) {
+    const update = {
+      ...profile,
+      updated_at: new Date(),
+    }
+
+    return this.supabase.from('profiles').upsert(update)
+  }
+
+  downLoadImage(path: string) {
+    return this.supabase.storage.from('avatars').download(path)
+  }
+
+  uploadAvatar(filePath: string, file: File) {
+    return this.supabase.storage.from('avatars').upload(filePath, file)
+  }
+
+}
---
+++ polity/src/app/core/database/10001_profile_table.sql
@@ -0,0 +1,23 @@
+-- Create a table for public profiles
+create table profiles (
+  id uuid references auth.users not null primary key,
+  updated_at timestamp with time zone,
+  username text unique,
+  full_name text,
+  avatar_url text,
+  website text,
+
+  constraint username_length check (char_length(username) >= 3)
+);
+
+alter table profiles
+  enable row level security;
+
+create policy "Public profiles are viewable by everyone." on profiles
+  for select using (true);
+
+create policy "Users can insert their own profile." on profiles
+  for insert with check (auth.uid() = id);
+
+create policy "Users can update own profile." on profiles
+  for update using (auth.uid() = id);
