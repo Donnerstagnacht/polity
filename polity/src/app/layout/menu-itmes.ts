45496b3252cf703f19bd98d96335893c98e940d8
Donnerstagnacht <tobias.hassebrock@gmail.com>
2023-10-14 12:34:43 +0200

    basic Taiga_UI and login page

---
+++ polity/src/app/layout/menu-itmes.ts
@@ -0,0 +1,31 @@
+type Item = {
+    badge?: number;
+    icon: string;
+    text: string;
+}
+
+const items: Item[] = [
+    {
+        text: 'VOR ORT',
+        icon: 'tuiIconUsers',
+        badge: 3,
+    },
+    {
+        text: 'SUCHE',
+        icon: 'tuiIconSearch',
+        badge: 1234,
+    },
+    {
+        text: 'NEU',
+        icon: 'tuiIconPlus',
+    },
+    {
+        text: 'ORGA',
+        icon: 'tuiIconCalendar',
+        badge: 100,
+    },
+    {
+        text: 'HOME',
+        icon: 'tuiIconHome',
+    },
+];
