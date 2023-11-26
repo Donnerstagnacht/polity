e6c6036b450196adbecbdfb68b150fea3c4293ec
Donnerstagnacht <tobias.hassebrock@gmail.com>
2023-11-26 02:58:31 +0100

    Standalone bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,20 @@ import {AuthenticationService} from "./auth/services/authentication.service";
 import {SessionStoreService} from "./auth/services/session-store.service";
 import {AuthChangeEvent, Session} from "@supabase/supabase-js";
 import {ErrorStoreService} from "./signal-store/error-store.service";
+import {RouterOutlet} from '@angular/router';
+import {TuiNotificationModule} from '@taiga-ui/core/components/notification';
+import {TuiRootModule} from '@taiga-ui/core';
 
 @Component({
     selector: 'polity-root',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.less']
+    styleUrls: ['./app.component.less'],
+    standalone: true,
+    imports: [
+        TuiRootModule,
+        TuiNotificationModule,
+        RouterOutlet
+    ]
 })
 export class AppComponent {
     showErrorMessage: WritableSignal<boolean>;
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,20 @@
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 
-import {AppModule} from './app/app.module';
+
+import { importProvidersFrom } from '@angular/core';
+import { AppComponent } from './app/app.component';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app/app-routing.module';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { NgDompurifySanitizer } from '@tinkoff/ng-dompurify';
+import { TUI_SANITIZER, TuiRootModule, TuiDialogModule, TuiAlertModule, TuiNotificationModule } from '@taiga-ui/core';
 
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+bootstrapApplication(AppComponent, {
+    providers: [
+        importProvidersFrom(BrowserModule, AppRoutingModule, TuiRootModule, TuiDialogModule, TuiAlertModule, TuiNotificationModule),
+        { provide: TUI_SANITIZER, useClass: NgDompurifySanitizer },
+        provideAnimations()
+    ]
+})
 .catch(err => console.error(err));
