38aba4e5256301169f9ea68750c3dd6ced48b6f9
Donnerstagnacht <tobias.hassebrock@gmail.com>
2023-12-20 13:44:07 +0100

    ng pwa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import {ErrorStoreService} from "./signal-store/error-store.service";
 import {RouterOutlet} from '@angular/router';
 import {TuiNotificationModule} from '@taiga-ui/core/components/notification';
 import {TuiRootModule} from '@taiga-ui/core';
+import {SwUpdate} from "@angular/service-worker";
 
 @Component({
     selector: 'polity-root',
@@ -26,12 +27,26 @@ export class AppComponent {
         private readonly authService: AuthenticationService,
         private sessionStoreService: SessionStoreService,
         private errorStoreService: ErrorStoreService,
+        private swUpdate: SwUpdate
     ) {
         this.authService.authChanges((_: AuthChangeEvent, session: Session | null): void => {
             this.sessionStoreService.setAuthData(session);
         })
         this.notification = this.errorStoreService.selectError();
         this.showErrorMessage = this.errorStoreService.selectShowError()
+    }
+
+    ngOnInit() {
+        if (this.swUpdate.isEnabled) {
+
+            this.swUpdate.versionUpdates.subscribe(() => {
+
+                if (confirm("New version available. Load New Version?")) {
+
+                    window.location.reload();
+                }
+            });
+        }
     }
 
     onClose(): void {
