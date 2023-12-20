c72280c4fd2815b86144eca45ee6dde76f28f27b
Donnerstagnacht <tobias.hassebrock@gmail.com>
2023-12-20 11:58:32 +0100

    ng pwa

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import {importProvidersFrom} from '@angular/core';
+import {importProvidersFrom, isDevMode} from '@angular/core';
 import {AppComponent} from './app/app.component';
 import {provideAnimations} from '@angular/platform-browser/animations';
 import {bootstrapApplication, BrowserModule} from '@angular/platform-browser';
@@ -6,25 +6,21 @@ import {NgDompurifySanitizer} from '@tinkoff/ng-dompurify';
 import {TUI_SANITIZER, TuiAlertModule, TuiDialogModule, TuiNotificationModule, TuiRootModule} from '@taiga-ui/core';
 import {provideRouter, withViewTransitions} from "@angular/router";
 import {APP_ROUTES} from "./app/app-routes";
+import { provideServiceWorker } from '@angular/service-worker';
 
 bootstrapApplication(AppComponent, {
     providers: [
-        importProvidersFrom(
-            BrowserModule,
-            TuiRootModule,
-            TuiDialogModule,
-            TuiAlertModule,
-            TuiNotificationModule
-        ),
-        {
-            provide: TUI_SANITIZER,
-            useClass: NgDompurifySanitizer
-        },
-        provideAnimations(),
-        provideRouter(
-            APP_ROUTES,
-            withViewTransitions()
-        )
-    ]
+    importProvidersFrom(BrowserModule, TuiRootModule, TuiDialogModule, TuiAlertModule, TuiNotificationModule),
+    {
+        provide: TUI_SANITIZER,
+        useClass: NgDompurifySanitizer
+    },
+    provideAnimations(),
+    provideRouter(APP_ROUTES, withViewTransitions()),
+    provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })
+]
 })
 .catch(err => console.error(err));
