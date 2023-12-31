7720567c5d983a0a0434d6dfeb2f680a125ea94d
Tobias Hassebrock <tobias.hassebrock@gmail.com>
2023-12-31 12:26:36 +0100

    Create 02000614_send_push_notification_trigger.sql

---
+++ supabase/migrations/06_notifications/02000614_send_push_notification_trigger.sql
@@ -0,0 +1,15 @@
+-- TODO: remove Bearer Key
+CREATE OR REPLACE TRIGGER "send-push-hook"
+    AFTER INSERT
+    ON authenticated_access.notifications_by_user
+    FOR EACH ROW
+EXECUTE FUNCTION supabase_functions.http_request(
+    'https://abcwkgkiztruxwvfwabf.supabase.co/functions/v1/send-push-notification',
+    'POST',
+    '{
+      "Content-Type":"application/json",
+      "Authorization": "Bearer <service role key>"
+      }',
+    '{}',
+    '1000'
+                 );
