debd22525dbf9471a9974f02c6e99f4875aa26cf
Donnerstagnacht <tobias.hassebrock@gmail.com>
2023-12-31 11:05:48 +0000

    Deploying to gh-pages from @ Donnerstagnacht/polity@fa6c903fa7af7a1ed26a2078d9137c54111ac7dd 🚀

---
+++ 569.37c23a01786099df.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkpolity=self.webpackChunkpolity||[]).push([[569],{3569:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
